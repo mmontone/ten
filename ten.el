84177094be0344e7334406be564ccb4f8abe4771
Mariano Montone <marianomontone@gmail.com>
2020-04-23 10:51:29 -0300

    Emacs minor mode
    Emacs minor mode with key bindings.
    
    C-c C-c to compile template.
    C-c RET to expand template.

--- ten.el
+++ ten.el
@@ -4,9 +4,10 @@
 
 (defun ten-expand-template ()
   (interactive)
-  (let ((expanded (slime-eval `(cl:with-output-to-string
-                          (s)
-                          (cl:pprint (ten:expand-template (cl::pathname ,(buffer-file-name))) s)))))
+  (let ((expanded (slime-eval
+                   `(cl:with-output-to-string
+                     (s)
+                     (cl:pprint (ten:expand-template (cl::pathname ,(buffer-file-name))) s)))))
     (slime-with-popup-buffer ("*TEN expansion*"
                               :package :ten-templates :connection t
                               :mode 'lisp-mode)
@@ -15,3 +16,15 @@
       (setq font-lock-keywords-case-fold-search t)
       (current-buffer)
       (insert expanded))))
+
+(define-minor-mode ten-mode
+  "Minor mode for compiling and expanding TEN templates"
+  :init-value nil
+  :lighter " TEN"
+  :keymap
+  `((,(kbd "C-c C-c") . ten-compile-template)
+    (,(kbd "C-c RET") . ten-expand-template))
+  :group 'ten)
+
+;; install like:
+;; (add-hook 'web-mode-hook 'ten-mode)
